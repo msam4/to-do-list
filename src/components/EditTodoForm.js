2032fc075f49f9316e38d2607df0ea825c764d2c
Michael Sam <sam.michael.h@gmail.com>
2023-12-14 20:51:40 +0900

    Add the delete and edit function

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const EditTodoForm = () => {
+export const EditTodoForm = ({editTodo, task}) => {
+  const [value, setValue] = useState(task.task)
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("");
+  }
+
   return (
-    <div>EditTodoForm</div>
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input type="text" className="todo-input" value={value} placeholder='Update Task' onChange={ (event) => setValue(event.target.value)} />
+      <button type='submit' className="todo-btn">Update Task</button>
+    </form>
   )
 }
