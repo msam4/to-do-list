432216c0c32fe7ce78543cc30527bd75e89db858
Michael Sam <sam.michael.h@gmail.com>
2023-12-14 20:16:12 +0900

    Add task to do list and cross out for completed

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    addTodo(value);
+
+    setValue("");
+  }
+
   return (
-    <div>TodoForm</div>
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input type="text" className="todo-input" value={value} placeholder='What is the task today?' onChange={ (event) => setValue(event.target.value)} />
+      <button type='submit' className="todo-btn">Add Task</button>
+    </form>
   )
 }
